a79a6e0b30fa6164e481453cf8875cd2eca2a544
KrithikShailesh <krithikshaileshr@gmail.com>
2021-05-27 12:42:39 +0530

    27/05/21

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        sc.close();
+
+        boolean swapped;
+
+        for(int i=0;i<n-1;i++){
+            swapped=false;
+            for(int j=0;j<n-i-1;j++){
+                
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped=true;
+                }
+
+            }
+            if(swapped=false){
+                break;
+            }
+
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
