63fe1b6521bfd3ffbd8fb4b07f01dc7c92e87527
KrithikShailesh <krithikshaileshr@gmail.com>
2021-05-28 22:53:48 +0530

    28/05/21

---
+++ QuickSort.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class QuickSort {
+    static void swap(int[] arr,int i,int j){
+
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+    }
+    static int partition(int[] arr,int low,int high){
+
+        int pivot=arr[high];
+
+        int i=low-1;
+
+        for(int j=low;j<=high-1;j++){
+
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+            
+        }
+        swap(arr,i+1,high);
+        return (i+1);
+    
+    }
+
+    static void sort(int[] arr,int low,int high){
+
+        if(low<high){
+            int pi=partition(arr,low,high);
+
+            sort(arr,low,pi-1);
+            sort(arr,pi+1,high);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sc.close();
+
+        sort(arr,0,n-1);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
